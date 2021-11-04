a81db84a2a1cbbd93b164a7ccee1059aa72ce19a
Caio Cacau <cacau.caio@gmail.com>
2021-11-04 10:22:44 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -18,5 +18,5 @@ public interface SaleRepository extends JpaRepository<Sale , Long> {
 	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSucessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
 			+ "FROM Sale AS obj GROUP BY obj.seller")
 	List<SaleSucessDTO> sucessGroupedBySeller();
-	
+
 }
