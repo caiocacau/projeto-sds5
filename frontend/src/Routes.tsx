6ebd93cb133951ca69e7dc571462f309cc4e364d
Caio Cacau <cacau.caio@gmail.com>
2021-11-05 17:35:13 -0300

    Ajustando arquivo Routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -7,11 +7,8 @@ function Rotas() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}>
-                </Route>
-                <Route path="/dashboard">
-                    <Dashboard />
-                </Route>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/dashboard" element={<Dashboard />}/>
             </Routes>
         </BrowserRouter>
     );
